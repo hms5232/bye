5500d3933c767a4d97c72a9fe10c7b0bde19b8d2
hms5232 <hms5232@hhming.moe>
2023-10-21 17:32:19 +0800

    feat: if windows...sorry!
    Currently not support Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
 extern crate ferris_says;
 
 use std::process::Command;
+#[cfg(not(target_os="windows"))]
 use std::os::unix::process::parent_id;
 use ferris_says::say;
 use std::io::{ stdout, BufWriter };
 
 fn main() {
-    let out = "Bye bye!\nI will pinch the connection.";
-    let width = 80;
+    let text = if cfg!(target_os = "windows") {
+        "Sorry!\nI can't pinch the connection on Windows currently."
+    } else {
+        "Bye bye!\nI will pinch the connection."
+    };
+    fsay(text, 80);
+    pinch();
+}
+
+/// Call ferris-says.
+///
+/// # Arguments
+///
+/// * `out` - What would says by Ferris
+/// * `width` - Max length of line
+fn fsay(out: &str, width: usize) {
     let mut writer = BufWriter::new(stdout());
     say(out, width, &mut writer).unwrap();
+}
 
-    Command::new("kill").args(["-s", "kill", &parent_id().to_string()]).output().expect("failed to execute process in bash");
+/// Close current connection
+fn pinch() {
+    #[cfg(not(target_os="windows"))]
+    Command::new("kill").args(["-s", "kill", &parent_id().to_string()]).output().expect("failed to pinch");
 }
